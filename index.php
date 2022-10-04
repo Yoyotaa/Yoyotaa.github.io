b297630e1216736071a271d3c524ed86c1f808e2
Godard <yoann1.godard@epitech.eu>
2022-10-04 15:24:43 +0200

    with index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>CGU - GACEC | Fabrication & Emballages Industriels</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/logo_gacec.png">
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/js/all.min.js"></script>
+    
+</head>
+<body>
+    <a href="./html/index.html">Voir le site</a>
+</body>
+</html>
